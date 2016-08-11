7d3688888e1e3c62a4254bb27e9e31ed00035412
lisa maccarrigan and sam peppard <sam@>
2016-08-11 15:23:16 -0400

    add new questions and outcomes

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,8 @@ $(document).ready(function() {
     var question2 = $("input:radio[name=question2]:checked").val();
     var question3 = $("input:radio[name=question3]:checked").val();
     var question4 = $("input:radio[name=question4]:checked").val();
+    var question5 = $("input:radio[name=question5]:checked").val();
+    var question6 = $("input:radio[name=question6]:checked").val();
 
     if (question1 === "agree") {
       politicalIndex++;
@@ -39,14 +41,34 @@ $(document).ready(function() {
       politicalIndex--;
     }
 
+    if (question5 === "agree") {
+      politicalIndex++;
+    }
+    else {
+      politicalIndex--;
+    }
+
+    if (question6 === "agree") {
+      politicalIndex++;
+    }
+    else {
+      politicalIndex--;
+    }
+
     if (politicalIndex === middle) {
       $(".outcome").text("Based on your responses " + name + ", you're a Moderate");
     }
+    else if (politicalIndex < middle && politicalIndex > -3) {
+      $(".outcome").text("Based on your responses " + name + ", you're kinda Liberal");
+    }
     else if (politicalIndex < middle) {
-      $(".outcome").text("Based on your responses " + name + ", you're a Liberal");
+      $(".outcome").text("Based on your responses " + name + ", you're very Liberal");
+    }
+    else if (politicalIndex > middle && politicalIndex < 3) {
+      $(".outcome").text("Based on your responses " + name + ", you're kinda Conservative");
     }
     else {
-      $(".outcome").text("Based on your responses " + name + ", you're a Conservative");
+      $(".outcome").text("Based on your responses " + name + ", you're very Conservative");
     }
 
   });
